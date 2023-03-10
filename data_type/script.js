b85fa4962c16ab1e32f3ac1e180a12c55e610d9b
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-10 14:17:28 +0700

    Data type now Object

--- data_type/script.js
+++ data_type/script.js
@@ -1,3 +1,82 @@
+// console.log('Hello World!');
+// let email = 'pramote.p.on@gmail.com';
+// console.log(email);
+
+/* 
+    String 
+*/
+let firstName = "Pramote";
+let lastName = "Phanon";
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+let info = `My name is ${fullName} and I an ${firstName}`;
+console.log(info);
+console.log(fullName.length);
+console.log(fullName.toLocaleUpperCase());
+
+/* 
+    Booleans
+*/
+// == Value, === Value and Datatype
+let email = 'pramote.p.on@gmail.com';
+let findLetter = email.includes('@');
+console.log(findLetter);
+
+/* 
+    Numbers
+*/
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
+console.log(10/2);
+let result = radius % 3;
+console.log(result);
+// Order B() I** D/ M* A+ S-
+let bidMas = 5 * (10-3)**2;
+console.log(bidMas);
+
+// Undefined The value of a variable [default: undefined]
+let isUndefined;
+console.log(isUndefined);
+// Null
+let age = null;
+console.log(age,age+3, `my age is ${age}`);
+// Null and Undefined
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+/* 
+    Array
+*/
+// [] is name square basket
+let selectedFruits = ['Apple', 'Banana', 'Orange'];
+console.log(selectedFruits); // [Apple, Banana, Orange]
+console.log(selectedFruits[1]); // Banana
+selectedFruits[3] = 'Pineapple';
+console.log(selectedFruits); // [Apple, Banana, Orange, Pineapple]
+selectedFruits[1] = 'Mango';
+console.log(selectedFruits); // [Apple, Mango, Orange, Pineapple]
+console.log(selectedFruits.length); // 4
+
+/* 
+    Object
+*/
+let user = {
+    firstName: 'Pramote',
+    lastName: 'Phanon',
+    email: 'pramote.p.on@gmail.com',
+    age: 28,
+    location: 'Bangkok',
+    readingList:['HTML', 'CSS', 'JavaScript'],
+}
+console.log(user); // Object List user
+console.log(user.email); // pramote.p.on@gmail.com
+console.log(user.age); // 28
+user.age = 29;
+console.log(user.age); // 29
+
+
+
 // Create function run
 function run(){
     var fruit = "banana"; // Variable var
@@ -20,4 +99,4 @@ function run(){
 }
 
 // Call Functiom run
-run();
+// run();
