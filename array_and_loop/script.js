8c8257dd1c7fe5e81cdb8205b774b3e3775d83ff
Zeekerz <pramote.p.on@gmail.com>
2023-03-15 13:42:09 +0700

    shift remove first data in array

--- array_and_loop/script.js
+++ array_and_loop/script.js
@@ -2,11 +2,11 @@
 const numbers = [3,4];
 console.log(numbers); // [3,4]
 
-// Method push
+// Method push add data to last data in array
 numbers.push(5,6);
 console.log(numbers); // [3,4,5,6]
 
-// Method unshift
+// Method unshift 
 numbers.unshift(1,2);
 console.log(numbers); // [1,2,3,4,5,6]
 
@@ -15,5 +15,12 @@ numbers.splice(2,0,'a','b');
 console.log(numbers); // [1, 2, 'a', 'b', 3, 4, 5, 6]
 
 // remove last data in array
-const last = numbers.pop();
+const removeLast = numbers.pop();
 console.log(numbers); // [1, 2, 'a', 'b', 3, 4, 5]
+// arr.pop return remove value
+console.log(removeLast); // 6
+
+// remove first data in array
+const removeFirst = numbers.shift();
+console.log(numbers); // [2, 'a', 'b', 3, 4, 5]
+console.log(removeFirst); // 1
