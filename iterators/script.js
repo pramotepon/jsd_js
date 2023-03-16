0fdab572d04e53c1e9aaba3f13c3833ee9fdec9d
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-16 13:34:22 +0700

    map

--- iterators/script.js
+++ iterators/script.js
@@ -2,24 +2,40 @@
     forEach
 */
 // Variable numbers type Array
-const numbers = [1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
 // numbers loop insert data to number
 numbers.forEach(number => {
-    console.log(number);
+    console.log(number); // 1 2 3 4 5
 });
 // numbers loop insert data to number
 numbers.forEach((item, index) => {
-    console.log(index, item);
+    console.log(index, item); // 0 1  1 2  2 3  3 4  4 5
 });
 
-const letters = ['a','b','c','d','a','c','a'];
+const letters = ['a', 'b', 'c', 'd', 'a', 'c', 'a'];
 let count = {}; // Create Variable count type Object
 console.log(typeof count);
 letters.forEach(item => {
     if (count[item]) { // check if have an letter
         count[item]++;
-    }else{
+    } else {
         count[item] = 1;
     }
 });
 console.log(count);
+
+/* 
+    map
+*/
+// storeUSD type Array
+let storeUSD = [5, 6, 7, 8];
+// storeUSD.map return value to variabel storeEUR
+let storeEUR = storeUSD.map(value => value *= 0.85);
+console.log(storeEUR); // [ 4.25, 5.1, 5.95, 6.8 ]
+
+function toEuros(value){
+    value *= 0.85;
+    return value;
+}
+let funcStoreEUR = storeUSD.map(toEuros);
+console.log(funcStoreEUR); // [ 4.25, 5.1, 5.95, 6.8 ]
