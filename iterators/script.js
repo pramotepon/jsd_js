c59672268fb0efc5de0a1450a508c1ddfe0bc4c4
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-16 14:07:19 +0700

    Reduce

--- iterators/script.js
+++ iterators/script.js
@@ -52,4 +52,14 @@ console.log(filtered); // [ 3, 4, 5 ]
 const lessNumber = numbers.findIndex(num => {
     return num < 10;
 });
-console.log(lessNumber);
+console.log(lessNumber); // 0
+
+/* 
+    Reduce
+*/
+// Accumulator is start value CurrentValue is End value 1 + 2... +5
+const sum = numbers.reduce((accumulator, currentValue) =>  accumulator + currentValue);
+console.log(sum); // 15
+// Can set Initial Value to Accumulator 10 + 1 + 2 ... +5
+const sumInitialValue = numbers.reduce((accumulator, currentValue) =>  accumulator + currentValue,10);
+console.log(sumInitialValue); // 25
