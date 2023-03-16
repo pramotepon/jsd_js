d944a244509c72eff2f2f8dd4328ab7fce001759
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-16 13:49:43 +0700

    Filter

--- iterators/script.js
+++ iterators/script.js
@@ -33,9 +33,15 @@ let storeUSD = [5, 6, 7, 8];
 let storeEUR = storeUSD.map(value => value *= 0.85);
 console.log(storeEUR); // [ 4.25, 5.1, 5.95, 6.8 ]
 
-function toEuros(value){
+function toEuros(value) {
     value *= 0.85;
     return value;
 }
 let funcStoreEUR = storeUSD.map(toEuros);
 console.log(funcStoreEUR); // [ 4.25, 5.1, 5.95, 6.8 ]
+
+/* 
+    Filter
+*/
+const filtered = numbers.filter(n => n >= 3);
+console.log(filtered); // [ 3, 4, 5 ]
